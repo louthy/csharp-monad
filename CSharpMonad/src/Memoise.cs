0e6ccb22667013e71ebd7bc3b50590bcd6883faa
louthy <p.louth@gmail.com>
2014-04-13 20:47:38 +0100

    Missing files again

---
+++ CSharpMonad/src/Memoise.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monad
+{
+    /// <summary>
+    /// Create a memoised value
+    /// </summary>
+    public static class Memo
+    {
+        /// <summary>
+        /// Create a memoised (cached) value, doesn't calculate the provided expression until the
+        /// value is required, and once it has been calculated it will cache the value for future
+        /// access.
+        /// </summary>
+        public static Memo<T> ise<T>(Func<T> func)
+        {
+            return new Memo<T>(func);
+        }
+    }
+
+    /// <summary>
+    /// Create a memoised (cached) value, doesn't calculate the provided expression until the
+    /// value is required, and once it has been calculated it will cache the value for future
+    /// access.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Memo<T>
+    {
+        readonly Func<T> func;
+        WeakReference reference;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="func">Function to invoke to get the value when needed</param>
+        public Memo(Func<T> func)
+        {
+            this.func = func;
+        }
+
+        /// <summary>
+        /// Access the value (this will cause the calculation if the value hasn't yet
+        /// been memoised).
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                if (reference == null)
+                {
+                    reference = new WeakReference(func());
+                }
+                else if (!reference.IsAlive)
+                {
+                    reference.Target = func();
+                }
+                return (T)reference.Target;
+            }
+        }
+
+        public bool HasValue
+        {
+            get
+            {
+                return reference != null && reference.IsAlive;
+            }
+        }
+
+        /// <summary>
+        /// Forget the memoised value
+        /// </summary>
+        public void Forget()
+        {
+            reference = null;
+        }
+
+        /// <summary>
+        /// Allows implicit conversion from the Memo<T> to T.
+        /// </summary>
+        public static implicit operator T(Memo<T> memo)
+        {
+            return memo.Value;
+        }
+    }
+}
---
+++ CSharpMonad/src/parsec/token/strings/Strings.cs
@@ -0,0 +1,114 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)
+// 
+// Copyright (c) 2014 Paul Louth
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monad.Parsec.Token.Strings
+{
+    public class StringLiteral : Parser<StringLiteralToken>
+    {
+        public StringLiteral()
+            :
+            base(
+                inp => (from l in Tok.Lexeme(
+                            from str in New.Between(
+                                New.Character('"'),
+                                New.Character('"').Fail("end of string"),
+                                New.Many( new StringChar() )
+                                )
+                            select str
+                        )
+                        select new StringLiteralToken(l,inp.First().Location)
+                       )
+                       .Fail("literal string")
+                       .Parse(inp)
+            )
+        { }
+    }
+
+    public class StringChar : Parser<ParserChar>
+    {
+        public StringChar()
+            :
+            base(
+                inp => (from c in new StringLetter()
+                        select c)
+                       .Or( new StringEscape() )
+                       .Fail("string character")
+                       .Parse(inp)
+            )
+        { }
+    }
+
+    public class StringLetter : Satisfy
+    {
+        public StringLetter()
+            :
+            base( c => (c != '"') && (c != '\\') && (c > 26), "string character" )
+        { }
+    }
+
+    public class StringEscape : Parser<ParserChar>
+    {
+        public StringEscape()
+            :
+            base(
+                inp =>
+                    New.Character('\\')
+                       .And(
+                            new EscapeGap().And(New.Failure<ParserChar>(ParserError.Create("", inp)))
+                           .Or(new EscapeEmpty().And(New.Failure<ParserChar>(ParserError.Create("", inp))))
+                           .Or(Tok.Chars.EscapeCode())
+                       )
+                       .Parse(inp)
+           )
+        { }
+    }
+
+    public class EscapeGap : Parser<ParserChar>
+    {
+        public EscapeGap()
+            :
+            base(
+                inp => (from sp in New.Many1(New.Character(' '))
+                        from ch in New.Character('\\')
+                        select ch)
+                       .Fail("end of string gap")
+                       .Parse(inp)
+            )
+        {}
+    }
+
+    public class EscapeEmpty : Character
+    {
+        public EscapeEmpty()
+            :
+            base('&')
+        { }
+    }
+}
