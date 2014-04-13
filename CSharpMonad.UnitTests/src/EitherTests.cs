fef56abf96ef4ea496e31a91647b3d9af97eea1d
louthy <p.louth@gmail.com>
2014-04-13 17:13:12 +0100

    Improved Either monad, with pattern matching methods and implicit conversion from the underlying types. Additional Either monad unit tests.

--- CSharpMonad.UnitTests/src/EitherTests.cs
+++ CSharpMonad.UnitTests/src/EitherTests.cs
@@ -8,47 +8,154 @@ namespace Monad.UnitTests
     public class EitherTests
     {
         [Test]
-        public void TestBinding1()
+        public void TestEitherBinding1()
         {
-            var r = 
-                from lhs in Either.Right<string, int>(2)
-                from  rhs in Either.Right<string, int>(2)
+            var r =
+                from lhs in Two()
+                from rhs in Two()
                 select lhs+rhs;
 
-            Assert.IsTrue(r.IsRight && r.GetRight() == 4);
+            Assert.IsTrue(r.IsRight && r.Right == 4);
         }
 
         [Test]
-        public void TestBinding2()
+        public void TestEitherBinding2()
         {
-            var r = 
-                from lhs in Either.Right<string, int>(2)
-                from rhs in Either.Left<string, int>("Error!!")
+            var r =
+                from lhs in Two()
+                from rhs in Error()
                 select lhs+rhs;
 
-            Assert.IsTrue(r.IsLeft && r.GetLeft() == "Error!!");
+            Assert.IsTrue(r.IsLeft && r.Left == "Error!!");
         }
 
 
         [Test]
-        public void TestBinding3()
+        public void TestEitherBinding3()
         {
             var r = 
-                from lhs in Either.Right<string, int>(2)
+                from lhs in Two()
                 select lhs;
 
-            Assert.IsTrue(r.IsRight && r.GetRight() == 2);
+            Assert.IsTrue(r.IsRight && r.Right == 2);
         }    
 
         [Test]
-        public void TestBinding4()
+        public void TestEitherBinding4()
         {
             var r = 
-                from lhs in Either.Left<string, int>("Error!!")
+                from lhs in Error()
                 select lhs;
 
-            Assert.IsTrue(r.IsLeft && r.GetLeft() == "Error!!");
-        }    
+            Assert.IsTrue(r.IsLeft && r.Left == "Error!!");
+        }
+
+        [Test]
+        public void TestEitherBinding5()
+        {
+            var r =
+                from one in Two()
+                from two in Error()
+                from thr in Two()
+                select one + two + thr;
+
+            Assert.IsTrue(r.IsLeft && r.Left == "Error!!");
+        }
+
+        [Test]
+        public void TestEitherMatch1()
+        {
+            var unit =
+                (from one in Two()
+                 from two in Error()
+                 from thr in Two()
+                 select one + two + thr)
+                .Match(
+                    Right: r => Assert.IsTrue(false),
+                    Left: l => Assert.IsTrue(l == "Error!!")
+                );
+        }
+
+        [Test]
+        public void TestEitherMatch2()
+        {
+            var unit =
+                (from one in Two()
+                 from two in Error()
+                 from thr in Two()
+                 select one + two + thr)
+                .Match(
+                    right => Assert.IsFalse(true),
+                    left => Assert.IsTrue(left == "Error!!")
+                );
+        }
+
+        [Test]
+        public void TestEitherMatch3()
+        {
+            var unit =
+                (from one in Two()
+                 from two in Two()
+                 select one + two)
+                .Match(
+                    Right: r => Assert.IsTrue(r == 4),
+                    Left: l => Assert.IsFalse(true)
+                );
+        }
+
+        [Test]
+        public void TestEitherMatch4()
+        {
+            var unit =
+                (from one in Two()
+                 from two in Two()
+                 select one + two)
+                .Match(
+                    right => Assert.IsTrue(right == 4),
+                    left => Assert.IsFalse(true)
+                );
+        }
+
+        [Test]
+        public void TestEitherMatch5()
+        {
+            var result =
+                (from one in Two()
+                 from two in Two()
+                 select one + two)
+                .Match(
+                    Right: r => r * 2,
+                    Left: l => 0
+                );
+
+            Assert.IsTrue(result == 8);
+        }
+
+        [Test]
+        public void TestEitherMatch6()
+        {
+            var result =
+                (from one in Two()
+                 from err in Error()
+                 from two in Two()
+                 select one + two)
+                .Match(
+                    Right: r => r * 2,
+                    Left: l => 0
+                );
+
+            Assert.IsTrue(result == 0);
+        }
+
+        public Either<string, int> Two()
+        {
+            return 2;
+        }
+
+        public Either<string, int> Error()
+        {
+            return "Error!!";
+        }
     }
 }
 
--- CSharpMonad/src/Either.cs
+++ CSharpMonad/src/Either.cs
@@ -2,41 +2,71 @@ using System;
 
 namespace Monad
 {
-    /*    
-
-        First pass at an Either monad, it's pretty crude and untested at the moment, so use with caution
-
-    */
-
+    /// <summary>
+    /// Either constructor methods
+    /// </summary>
     public class Either
     {
+        /// <summary>
+        /// Construct an Either Left monad
+        /// </summary>
         public static Either<L,R> Left<L,R>(L left)
         {
             return new Either<L,R>(left);
         }
-        public static Either<L,R> Right<L,R>(R right)
+        /// <summary>
+        /// Construct an Either Right monad
+        /// </summary>
+        public static Either<L, R> Right<L, R>(R right)
         {
             return new Either<L,R>(right);
         }
     }
 
+    /// <summary>
+    /// The Either monad represents values with two possibilities: a value of Left or Right
+    /// Either is sometimes used to represent a value which is either correct or an error, 
+    /// by convention, 'Left' is used to hold an error value 'Right' is used to hold a 
+    /// correct value.
+    /// So you can see that Either has a very close relationship to the Error monad.  However,
+    /// the Either monad won't capture exceptions.  Either would primarily be used for 
+    /// known error values rather than exceptional ones.
+    /// Once the Either monad is in the Left state it cancels the monad bind function and 
+    /// returns immediately.
+    /// </summary>
+    /// <typeparam name="L"></typeparam>
+    /// <typeparam name="R"></typeparam>
     public class Either<L,R>
     {
-        public readonly object Value;
+        readonly L left;
+        readonly R right;
+
+        /// <summary>
+        /// Returns true if the monad object is in the Left state
+        /// </summary>
         public readonly bool IsLeft;
 
-        public Either(L left)
+        /// <summary>
+        /// Left constructor
+        /// </summary>
+        internal Either(L left)
         {
             IsLeft = true;
-            Value = left;
+            this.left = left;
         }
 
-        public Either(R right)
+        /// <summary>
+        /// Right constructor
+        /// </summary>
+        internal Either(R right)
         {
             IsLeft = false;
-            Value = right;
+            this.right = right;
         }
 
+        /// <summary>
+        /// Returns true if the monad object is in the Right state
+        /// </summary>
         public bool IsRight
         {
             get
@@ -45,53 +75,192 @@ namespace Monad
             }
         }
 
-        public L GetLeft()
+        /// <summary>
+        /// Get the Left value
+        /// NOTE: This throws an InvalidOperationException if the object is in the 
+        /// Right state
+        /// </summary>
+        public L Left
         {
-            if (!IsLeft)
-                throw new InvalidOperationException("Not in a left state");
-            return (L)Value;
+            get
+            {
+                if (!IsLeft)
+                    throw new InvalidOperationException("Not in the left state");
+                return left;
+            }
         }
 
-        public R GetRight()
+        /// <summary>
+        /// Get the Right value
+        /// NOTE: This throws an InvalidOperationException if the object is in the 
+        /// Left state
+        /// </summary>
+        public R Right
         {
-            if (!IsRight)
-                throw new InvalidOperationException("Not in a right state");
-            return (R)Value;
+            get
+            {
+                if (!IsRight)
+                    throw new InvalidOperationException("Not in the right state");
+                return right;
+            }
+        }
+
+        /// <summary>
+        /// Implicit left operator conversion
+        /// </summary>
+        public static implicit operator Either<L, R>(L left)
+        {
+            return new Either<L, R>(left);
+        }
+
+        /// <summary>
+        /// Implicit right operator conversion
+        /// </summary>
+        public static implicit operator Either<L, R>(R right)
+        {
+            return new Either<L, R>(right);
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// </summary>
+        /// <param name="Right">Action to perform if the monad is in the Right state</param>
+        /// <param name="Left">Action to perform if the monad is in the Left state</param>
+        /// <returns>T</returns>
+        public T Match<T>(Func<R,T> Right, Func<L,T> Left)
+        {
+            return IsLeft
+                ? Left(this.Left)
+                : Right(this.Right);
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// NOTE: This throws an InvalidOperationException if the object is in the 
+        /// Left state
+        /// </summary>
+        /// <param name="right">Action to perform if the monad is in the Right state</param>
+        /// <returns>T</returns>
+        public T MatchRight<T>(Func<R, T> right)
+        {
+            return right(this.Right);
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// NOTE: This throws an InvalidOperationException if the object is in the 
+        /// Right state
+        /// </summary>
+        /// <param name="left">Action to perform if the monad is in the Left state</param>
+        /// <returns>T</returns>
+        public T MatchLeft<T>(Func<L, T> left)
+        {
+            return left(this.Left);
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// Returns the defaultValue if the monad is in the Left state
+        /// </summary>
+        /// <param name="right">Action to perform if the monad is in the Right state</param>
+        /// <returns>T</returns>
+        public T MatchRight<T>(Func<R, T> right, T defaultValue)
+        {
+            if (IsLeft) 
+                return defaultValue;
+            return right(this.Right);
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// Returns the defaultValue if the monad is in the Right state
+        /// </summary>
+        /// <param name="left">Action to perform if the monad is in the Left state</param>
+        /// <returns>T</returns>
+        public T MatchLeft<T>(Func<L, T> left, T defaultValue)
+        {
+            if (IsRight)
+                return defaultValue;
+            return left(this.Left);
+        }
+
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// </summary>
+        /// <param name="Right">Action to perform if the monad is in the Right state</param>
+        /// <param name="Left">Action to perform if the monad is in the Left state</param>
+        /// <returns>Unit</returns>
+        public Unit Match(Action<R> Right, Action<L> Left)
+        {
+            return Unit.Return( () =>
+            {
+                if (IsLeft)
+                    Left(this.Left);
+                else
+                    Right(this.Right);
+            });
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// NOTE: This throws an InvalidOperationException if the object is in the 
+        /// Left state
+        /// </summary>
+        /// <param name="right">Action to perform if the monad is in the Right state</param>
+        /// <returns>Unit</returns>
+        public Unit MatchRight(Action<R> right)
+        {
+            return Unit.Return(() => right(this.Right));
+        }
+
+        /// <summary>
+        /// Pattern matching method for a branching expression
+        /// NOTE: This throws an InvalidOperationException if the object is in the 
+        /// Right state
+        /// </summary>
+        /// <param name="left">Action to perform if the monad is in the Left state</param>
+        /// <returns>Unit</returns>
+        public Unit MatchLeft(Action<L> left)
+        {
+            return Unit.Return(() => left(this.Left));
         }
     }
 
+    /// <summary>
+    /// Either extension methods
+    /// </summary>
     public static class EitherExt
     {
+        /// <summary>
+        /// Select
+        /// </summary>
         public static Either<L, UR> Select<TR, UR, L>(
             this Either<L, TR> self, 
             Func<TR, UR> selector)
         {
-            if (self.IsLeft) return Either.Left<L,UR>(
-                (L)self.Value
-            );
+            if (self.IsLeft) 
+                return Either.Left<L,UR>( self.Left );
 
-            var val = (TR)self.Value;
-            return Either.Right<L,UR>( selector( val ) );
+            return Either.Right<L,UR>( selector( self.Right ) );
         }
 
+        /// <summary>
+        /// SelectMany
+        /// </summary>
         public static Either<L, VR> SelectMany<TR, UR, VR, L>(
             this Either<L, TR> self, 
             Func<TR, Either<L, UR>> selector, 
             Func<TR,UR,VR> projector)
         {
-            if (self.IsLeft) return Either.Left<L,VR>(
-                (L)self.Value
-            );
+            if (self.IsLeft) 
+                return Either.Left<L,VR>(self.Left);
 
-            var val = (TR)self.Value;
-            var res2 = selector( val );
-            if (res2.IsLeft) return Either.Left<L,VR>(
-                (L)res2.Value
-            );
+            var res = selector(self.Right);
+            if (res.IsLeft) 
+                return Either.Left<L,VR>(res.Left);
 
-            var val2 = (UR)res2.Value;
-
-            return Either.Right<L,VR>(projector(val, val2));
+            return Either.Right<L, VR>(projector(self.Right, res.Right));
         }
     }
 }
