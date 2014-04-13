8b021501aefd13f9c319e569f0d4131f76c51fe8
louthy <p.louth@gmail.com>
2014-04-13 03:15:16 +0100

    Missing files

---
+++ CSharpMonad.UnitTests/src/LexerTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Monad;
+using Monad.Parsec;
+using Monad.Parsec.Language;
+using Monad.Parsec.Token;
+
+namespace CSharpMonad.UnitTests.src
+{
+    [TestFixture]
+    public class LexerTests
+    {
+        [Test]
+        public void Haskell98LexerTest()
+        {
+            var lexer = new TokenParser(new Haskell98Def());
+
+            var result = lexer.Parse(
+@"call2 :: Expr -> Parser Expr
+call2 lhs = do
+    rhs <- expr
+    rhsr <- call2 rhs
+    return $ Call lhs rhsr"
+);
+
+            if (result.IsFaulted)
+            {
+                string errs = System.String.Join("\n",
+                    result.Errors.Select(e =>
+                        e.Message + "Expected " + e.Expected + " at " + e.Location +
+                        " - " + e.Input.AsString().Substring(0, Math.Min(30, e.Input.AsString().Length))
+                        + "...")
+                    );
+                Console.WriteLine(errs);
+            }
+
+            Assert.IsTrue(!result.IsFaulted);
+        }
+    }
+}
---
+++ CSharpMonad/src/Unit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monad
+{
+    /// <summary>
+    /// A type with only one value, itself.
+    /// This is the functional world's equivalent of Void.
+    /// </summary>
+    public class Unit
+    {
+        /// <summary>
+        /// Use a singleton so that ref equality works and to reduce any unnecessary
+        /// thrashing of the GC from creating an object which is always the same.
+        /// </summary>
+        private static Unit singleton = new Unit();
+
+        /// <summary>
+        /// Private ctor
+        /// </summary>
+        private Unit()
+        {
+        }
+
+        /// <summary>
+        /// Return
+        /// </summary>
+        public static Unit Return()
+        {
+            return singleton;
+        }
+
+        /// <summary>
+        /// Performs an action which instead of returning void will return Unit
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static Unit Return(Action action)
+        {
+            action();
+            return Return();
+        }
+    }
+}
