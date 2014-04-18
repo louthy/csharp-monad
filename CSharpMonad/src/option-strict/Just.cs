cf8bb5cd95fc9a2a6d6177ce80742832f0c75d76
louthy <p.louth@gmail.com>
2014-04-18 20:11:16 +0100

    Re-added the old Option<T> type as OptionStrict<T>

---
+++ CSharpMonad/src/option-strict/Just.cs
@@ -0,0 +1,142 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)
+// 
+// Copyright (c) 2014 Paul Louth
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monad
+{
+    /// <summary>
+    /// Just case of the Option<T> monad
+    /// </summary>
+    public class JustStrict<T> : OptionStrict<T>
+    {
+        static readonly string TypeOfT = typeof(T).ToString();
+        static readonly bool IsAppendable = typeof(IAppendable<T>).IsAssignableFrom(typeof(T));
+        private readonly T value;
+
+        public JustStrict(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public override T Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public override bool HasValue
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override R Match<R>(Func<R> Just, Func<R> Nothing)
+        {
+            return Just();
+        }
+
+        public override R Match<R>(Func<T, R> Just, Func<R> Nothing)
+        {
+            return Just(Value);
+        }
+
+        public override R Match<R>(Func<R> Just, R Nothing)
+        {
+            return Just();
+        }
+
+        public override R Match<R>(Func<T, R> Just, R Nothing)
+        {
+            return Just(Value);
+        }
+
+
+        /// <summary>
+        /// Monadic append
+        /// If the lhs or rhs are in a Nothing state then Nothing propagates
+        /// </summary>
+        public override OptionStrict<T> Mappend(OptionStrict<T> rhs)
+        {
+            if (!rhs.HasValue)
+            {
+                return rhs;
+            }
+            else
+            {
+                if (IsAppendable)
+                {
+                    var lhs = this.Value as IAppendable<T>;
+                    return new JustStrict<T>(lhs.Append(rhs.Value));
+                }
+                else
+                {
+                    // TODO: Consider replacing this with a Reflection.Emit which does this job efficiently.
+                    switch (TypeOfT)
+                    {
+                        case "System.Int64":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToInt64(Value) + Convert.ToInt64(rhs.Value)), typeof(T)));
+                        case "System.UInt64":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToUInt64(Value) + Convert.ToUInt64(rhs.Value)), typeof(T)));
+                        case "System.Int32":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToInt32(Value) + Convert.ToInt32(rhs.Value)), typeof(T)));
+                        case "System.UInt32":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToUInt32(Value) + Convert.ToUInt32(rhs.Value)), typeof(T)));
+                        case "System.Int16":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToInt16(Value) + Convert.ToInt16(rhs.Value)), typeof(T)));
+                        case "System.UInt16":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToUInt16(Value) + Convert.ToUInt16(rhs.Value)), typeof(T)));
+                        case "System.Decimal":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToDecimal(Value) + Convert.ToDecimal(rhs.Value)), typeof(T)));
+                        case "System.Double":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToDouble(Value) + Convert.ToDouble(rhs.Value)), typeof(T)));
+                        case "System.Single":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToSingle(Value) + Convert.ToSingle(rhs.Value)), typeof(T)));
+                        case "System.Char":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToChar(Value) + Convert.ToChar(rhs.Value)), typeof(T)));
+                        case "System.Byte":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToByte(Value) + Convert.ToByte(rhs.Value)), typeof(T)));
+                        case "System.String":
+                            return new JustStrict<T>((T)Convert.ChangeType((Convert.ToString(Value) + Convert.ToString(rhs.Value)), typeof(T)));
+                        default:
+                            throw new InvalidOperationException("Type " + typeof(T).Name + " is not appendable.  Consider implementing the IAppendable interface.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharpMonad/src/option-strict/OptionStrict.cs
@@ -0,0 +1,328 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)
+// 
+// Copyright (c) 2014 Paul Louth
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monad
+{
+    /// <summary>
+    /// Option monad
+    /// </summary>
+    public abstract class OptionStrict<T> : IEquatable<OptionStrict<T>>, IEquatable<T>
+    {
+        /// <summary>
+        /// Represents a Option monad without a value
+        /// </summary>
+        public readonly static OptionStrict<T> Nothing = new NothingStrict<T>();
+
+        /// <summary>
+        /// Conversion from any value to Option<T>
+        /// Null is always considered as Nothing
+        /// </summary>
+        public static implicit operator OptionStrict<T>(T value)
+        {
+            return value.ToOptionStrict();
+        }
+
+        /// <summary>
+        /// Monad value
+        /// </summary>
+        public abstract T Value
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Does the monad have a value
+        /// </summary>
+        public abstract bool HasValue
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Get the monad's value or the default value for the type
+        /// </summary>
+        /// <returns></returns>
+        public T GetValueOrDefault()
+        {
+            return HasValue ? Value : default(T);
+        }
+
+        /// <summary>
+        /// Executes the delegate related to the derived Option type.
+        /// </summary>
+        public abstract R Match<R>(Func<R> Just, Func<R> Nothing);
+
+        /// <summary>
+        /// Executes the delegate related to the derived Option type.
+        /// </summary>
+        public abstract R Match<R>(Func<T, R> Just, Func<R> Nothing);
+
+        /// <summary>
+        /// Executes the delegate related to the derived Option type.
+        /// </summary>
+        public abstract R Match<R>(Func<R> Just, R Nothing);
+
+        /// <summary>
+        /// Executes the delegate related to the derived Option type.
+        /// </summary>
+        public abstract R Match<R>(Func<T, R> Just, R Nothing);
+
+        /// <summary>
+        /// Mappend
+        /// If the left-hand side or right-hand side are in a Left state, then Left propogates
+        /// </summary>
+        public static OptionStrict<T> operator +(OptionStrict<T> lhs, OptionStrict<T> rhs)
+        {
+            return lhs.Mappend(rhs);
+        }
+
+        /// <summary>
+        /// Nothing coalescing operator
+        /// Returns the left-hand operand if the operand is not Nothing; otherwise it returns the right hand operand.
+        /// In other words it returns the first valid option in the operand sequence.
+        /// </summary>
+        public static OptionStrict<T> operator |(OptionStrict<T> lhs, OptionStrict<T> rhs)
+        {
+            return lhs.HasValue
+                ? lhs
+                : rhs;
+        }
+
+        /// <summary>
+        /// Returns the right-hand side if the left-hand and right-hand side are not Nothing.
+        /// In order words every operand must hold a value for the result to be Just.
+        /// </summary>
+        public static OptionStrict<T> operator &(OptionStrict<T> lhs, OptionStrict<T> rhs)
+        {
+            return lhs.HasValue && rhs.HasValue
+                ? rhs
+                : lhs.HasValue
+                    ? rhs
+                    : lhs;
+        }
+
+        /// <summary>
+        /// Equals override
+        /// Compares the underlying values
+        /// </summary>
+        public static bool operator ==(OptionStrict<T> lhs, OptionStrict<T> rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        /// <summary>
+        /// Not equals override
+        /// Compares the underlying values
+        /// </summary>
+        public static bool operator !=(OptionStrict<T> lhs, OptionStrict<T> rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        /// <summary>
+        /// Monadic append
+        /// If the lhs or rhs are in a Nothing state then Nothing propagates
+        /// </summary>
+        public abstract OptionStrict<T> Mappend(OptionStrict<T> rhs);
+
+        /// <summary>
+        /// Converts the Option to an enumerable
+        /// </summary>
+        /// <returns>
+        /// Just: A list with one T in
+        /// Nothing: An empty list
+        /// </returns>
+        public IEnumerable<T> AsEnumerable()
+        {
+            if (HasValue)
+                yield return Value;
+            else
+                yield break;
+        }
+
+        /// <summary>
+        /// Converts the Option to an infinite enumerable
+        /// </summary>
+        /// <returns>
+        /// Just: An infinite list of T
+        /// Nothing: An empty list
+        /// </returns>
+        public IEnumerable<T> AsEnumerableInfinte()
+        {
+            if (HasValue)
+                while (true) yield return Value;
+            else
+                yield break;
+        }
+
+        /// <summary>
+        /// GetHashCode
+        /// </summary>
+        /// <returns>Just.GetHashCode() or Nothing.GetHashCode()</returns>
+        public override int GetHashCode()
+        {
+            return HasValue
+                ? Value == null ? 0 : Value.GetHashCode()
+                : Nothing.GetHashCode();
+        }
+
+        /// <summary>
+        /// Equals override
+        /// Compares the underlying values
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (obj is OptionStrict<T>)
+                {
+                    var rhs = (OptionStrict<T>)obj;
+                    return HasValue && rhs.HasValue
+                        ? Value.Equals(rhs.Value)
+                        : !HasValue && !rhs.HasValue
+                            ? true
+                            : false;
+                }
+                else if (obj is T)
+                {
+                    var rhs = (T)obj;
+                    return HasValue
+                        ? Value.Equals(rhs)
+                        : false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Equals override
+        /// Compares the underlying values
+        /// </summary>
+        public bool Equals(OptionStrict<T> rhs)
+        {
+            return Equals((object)rhs);
+        }
+
+        /// <summary>
+        /// Equals override
+        /// Compares the underlying values
+        /// </summary>
+        public bool Equals(T rhs)
+        {
+            return Equals((object)rhs);
+        }
+    }
+
+    /// <summary>
+    /// Option<T> extensions
+    /// </summary>
+    public static class OptionStrict
+    {
+        /// <summary>
+        /// Monadic zero
+        /// </summary>
+        public static OptionStrict<T> Mempty<T>()
+        {
+            return new JustStrict<T>(default(T));
+        }
+    }
+
+    /// <summary>
+    /// Option<T> monad extension methods
+    /// </summary>
+    public static class OptionStrictExtensions
+    {
+        /// <summary>
+        /// Converts this object to a Option monad.
+        /// Tretas null as Nothing
+        /// </summary>
+        /// <returns>Option<T></returns>
+        public static OptionStrict<T> ToOptionStrict<T>(this T self)
+        {
+            return self == null
+                ? OptionStrict<T>.Nothing
+                : new JustStrict<T>(self);
+        }
+
+        public static OptionStrict<R> Select<T, R>(this OptionStrict<T> self, Func<T, R> map)
+        {
+            return self.HasValue
+                ? map(self.Value).ToOptionStrict()
+                : OptionStrict<R>.Nothing;
+        }
+
+        public static OptionStrict<U> SelectMany<T, U>(this OptionStrict<T> self, Func<T, OptionStrict<U>> k)
+        {
+            return self.HasValue
+                ? k(self.Value)
+                : OptionStrict<U>.Nothing;
+        }
+
+        public static OptionStrict<V> SelectMany<T, U, V>(
+            this OptionStrict<T> self,
+            Func<T, OptionStrict<U>> select,
+            Func<T, U, V> project
+            )
+        {
+            if (!self.HasValue)
+                return OptionStrict<V>.Nothing;
+
+            var res = select(self.Value);
+            if (!res.HasValue)
+                return OptionStrict<V>.Nothing;
+
+            return new JustStrict<V>(project(self.Value, res.Value));
+        }
+
+        /// <summary>
+        /// Mconcat
+        /// </summary>
+        public static OptionStrict<T> Mconcat<T>(this IEnumerable<OptionStrict<T>> ms)
+        {
+            var value = ms.Head();
+
+            foreach (var m in ms.Tail())
+            {
+                if (!value.HasValue)
+                    return value;
+
+                value = value.Mappend(m);
+            }
+            return value;
+        }
+    }
+}
